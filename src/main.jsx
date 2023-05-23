2db81e7231324d48ba764dde02ba48afdb74772f
Markone Dias <markdiasdev1@gmail.com>
2023-05-23 11:54:10 -0300

    atualizei

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
