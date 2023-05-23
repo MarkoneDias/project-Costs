ca58e0a25a1e8eeed38c4979a39f32672ccfcc58
Markone Dias <markdiasdev1@gmail.com>
2023-05-23 11:31:49 -0300

    adcionei o browser

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
